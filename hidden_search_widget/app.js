18d15254f061bf68fcb738c27cc5ca6de913b880
Lael Aichaoui <lael.aichaoui@gmail.com>
2023-01-19 16:21:38 -0800

    js functionnality added

---
+++ hidden_search_widget/app.js
@@ -0,0 +1,4 @@
+const btn = document.querySelector(".btn");
+const searchContainer = document.querySelector(".search-container");
+
+btn.addEventListener("click", () => searchContainer.classList.toggle("active"));
